952317c660f0f2c765f5a913959d9e32ecde0455
Denis Koltsov <iam@mistadikay.com>
2018-07-01 00:18:35 +0200

    :chart_with_upwards_trend: Add Google Analytics

---
+++ gatsby-config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  plugins: [
+    {
+      resolve: "gatsby-plugin-google-analytics",
+      options: {
+        trackingId: "UA-12099885-2",
+        head: false,
+        anonymize: true
+      }
+    }
+  ]
+};
