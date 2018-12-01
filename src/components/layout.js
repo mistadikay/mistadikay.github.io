6fc798159cea38cc0b23f5757d644573b405a48b
Denis Koltsov <iam@mistadikay.com>
2018-12-01 20:41:48 +0100

    :bug: Fix missing global styles in "movies"

--- src/components/layout.js
+++ src/components/layout.js
@@ -12,6 +12,28 @@ const Layout = ({ children }) => (
         content="VaTl-665VAXK5f8XfQqBeAyYW71Y91rhpF4AvwsBVZY"
       />
       <link rel="icon" type="image/png" href={favicon} />
+      <style type="text/css">
+        {`
+        html,
+        body {
+          height: 100%;
+        }
+
+        body {
+          font-family: Helvetica, Arial, sans-serif;
+          background: #fff;
+          color: #333;
+          font-size: 14px;
+          line-height: 22px;
+          margin: 0;
+        }
+
+        #___gatsby,
+        #___gatsby > div {
+          height: 100%;
+        }
+      `}
+      </style>
     </Helmet>
     {children}
   </>
