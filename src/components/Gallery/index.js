e31eb3ddb0cbd1575c11c1f5c424be02058b3e4e
Denis Koltsov <iam@mistadikay.com>
2021-04-27 19:36:00 +0200

    Refactor GalleryComposition component to a functional one

--- src/components/Gallery/index.js
+++ src/components/Gallery/index.js
@@ -1,46 +1,35 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import Lightbox from "react-images";
 
-class GalleryComposition extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      currentImage: 0,
-      images: props.images.map(({ caption, image }) => ({
-        ...image.childImageSharp.fluid,
-        caption,
-      })),
-    };
+const GalleryComposition = (props) => {
+  // https://github.com/jossmac/react-images/issues/247
+  if (typeof document === "undefined") {
+    return null;
   }
 
-  render() {
-    const { images, currentImage } = this.state;
+  const [currentImage, setCurrentImage] = useState(0);
+  const images = props.images.map(({ caption, image }) => ({
+    ...image.childImageSharp.fluid,
+    caption,
+  }));
 
-    // https://github.com/jossmac/react-images/issues/247
-    if (typeof document === "undefined") {
-      return null;
-    }
-
-    return (
-      <Lightbox
-        backdropClosesModal
-        enableKeyboardInput
-        showImageCount
-        showThumbnails
-        imageCountSeparator="/"
-        views={images}
-        preloadNextImage
-        currentImage={currentImage}
-        isOpen
-        onClickThumbnail={(imageIndex) =>
-          this.setState({ currentImage: imageIndex })
-        }
-        onClickPrev={() => this.setState({ currentImage: currentImage - 1 })}
-        onClickNext={() => this.setState({ currentImage: currentImage + 1 })}
-        onClose={this.props.onClose}
-      />
-    );
-  }
-}
+  return (
+    <Lightbox
+      backdropClosesModal
+      enableKeyboardInput
+      showImageCount
+      showThumbnails
+      imageCountSeparator="/"
+      views={images}
+      preloadNextImage
+      currentImage={currentImage}
+      isOpen
+      onClickThumbnail={(imageIndex) => setCurrentImage(imageIndex)}
+      onClickPrev={() => setCurrentImage(currentImage - 1)}
+      onClickNext={() => setCurrentImage(currentImage + 1)}
+      onClose={props.onClose}
+    />
+  );
+};
 
 export default GalleryComposition;
